d74451c6c08747e55358396ed99cfa701778d2d8
shifa-Alam <shifa7446@gmail.com>
2021-09-25 00:26:36 +0600

    initial

---
+++ oscilloSoftTest/solutionForProb2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solutionForProb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target_value;
+            var indexes = string.Empty;
+
+            Console.WriteLine("Write nums with specific format like [1,2,3]: ");
+            var input = Console.ReadLine();
+            input =input.Remove(0, 1);
+            input = input.Remove(input.Length-1);
+
+            string[] input_nums = input.Split(",");
+
+            Console.WriteLine("Enter Terget Value:");
+            target_value = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < input_nums.Length; i++)
+            {
+                for (int j = i + 1; j < input_nums.Length; j++)
+                {
+                    if (int.Parse(input_nums[i].ToString()) + int.Parse(input_nums[j].ToString()) == target_value)
+                    {
+                        indexes = indexes + $",{Array.IndexOf(input_nums, input_nums[i])},{Array.IndexOf(input_nums, input_nums[j])}";
+                    }
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(indexes))
+            {
+                indexes = string.Equals(indexes[0].ToString(), ",") ? indexes.Remove(0, 1) : indexes.Trim();
+
+            }
+            if (!string.IsNullOrWhiteSpace(indexes) && indexes.Length > 1)
+            {
+                indexes = string.Equals(indexes[indexes.Length - 1].ToString(), ",") ? indexes.Remove(indexes.Length - 1) : indexes.Trim();
+            }
+            Console.WriteLine("[" + indexes + "]");
+        }
+    }
+}
---
+++ oscilloSoftTest/solutionForProblem3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace solutionForProblem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = Console.ReadLine();
+            var num2 = Console.ReadLine();
+
+            num1 = reverseString(num1);
+            num2 = reverseString(num2);
+            var result = Add(num1, num2);
+
+            result = reverseString(result);
+            var firstItem = result[0].ToString();
+            if (string.Equals(firstItem, "0"))
+            {
+                result = result.Remove(0, 1);
+            }
+            Console.WriteLine(result);
+
+        }
+        public static string reverseString(string s)
+        {
+            var finalString = string.Empty;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                finalString = finalString + string.Concat(s[i]);
+            }
+            return finalString;
+        }
+
+
+        private static string Add(string a, string b)
+        {
+            //var valid = ApplyValidation(int.Parse(a), int.Parse(b));
+            //if (!valid) return "Invalid Argument";
+
+            var extraVal = 0;
+            var val = string.Empty;
+            for (var i = a.Length - 1; i >= 0; i--)
+            {
+                var firstNumber = int.Parse(a[i].ToString());
+                var secondNumber = int.Parse(b[i].ToString()) + extraVal;
+
+                if (firstNumber + secondNumber >= 10)
+                {
+                    var rowVal = (firstNumber + secondNumber).ToString();
+                    val = rowVal[1].ToString() + val;
+                    extraVal = int.Parse(rowVal[0].ToString());
+                }
+                else
+                {
+                    val = (firstNumber + secondNumber).ToString() + val;
+                    extraVal = 0;
+                }
+            }
+            if (extraVal > 0)
+            {
+                return extraVal.ToString() + val;
+            }
+            return val;
+        }
+
+        //private static bool ApplyValidation(int num1, int vnum2)
+        //{
+        //    if (num1 < 0 && vnum2 < 0) return false;
+        //    return true;
+        //}
+    }
+}
